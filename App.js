232d02be4cbc3e37a59dbb521f600b5ca2521c12
Vahram Grigoryan <ramgrigoryan@gmail.com>
2023-08-10 18:31:33 +0400

    styling: first iteration

--- App.js
+++ App.js
@@ -5,10 +5,12 @@ export default function App() {
     <View style={styles.appContainer}>
       <View style={styles.userInteractionBlock}>
         <TextInput style={styles.textInput} placeholder="Write your goal" />
-        <Button title="Create a goal" />
+        <View style={styles.button}>
+          <Button title="Create a goal" />
+        </View>
       </View>
-      <View>
-        <Text>List of your goals</Text>
+      <View style={styles.dataBlock}>
+        <Text style={styles.goalBlockTitle}>List of your goals</Text>
       </View>
     </View>
   );
@@ -16,20 +18,35 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    paddingVertical: 50,
+    paddingHorizontal: 12,
+    flex: 1,
   },
   userInteractionBlock: {
+    flex: 1,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
     gap: 20,
-    marginBottom: 50,
+    paddingBottom: 10,
+    borderBottomWidth: 1,
+    borderColor: "#EEF",
+  },
+  dataBlock: {
+    flex: 5,
+    paddingTop: 20,
+  },
+  goalBlockTitle: {
+    alignSelf: "center",
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#DEF",
     padding: 5,
-    width: "50%",
+    width: "60%",
     overflow: "hidden",
+  },
+  button: {
+    fontSize: 10,
   },
 });
